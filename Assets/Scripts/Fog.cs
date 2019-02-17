8e41160a800f3c8aae9567e50d1cd290a509015a
Soul Evans <soulevans07@gmail.com>
2019-02-17 01:07:50 +0100

    box human and fog

---
+++ Assets/Scripts/Fog.cs
@@ -0,0 +1,96 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Fog : MonoBehaviour {
+    private Transform _transform;
+    private Vector2 _pos;
+    private Camera _camera;
+    public Rect body;
+    public float _diff;
+    public float minDiff = 1.5f;
+    public float maxDiff = 0.5f;
+    public float minScale = 1.0f;
+    public float maxScale = 4.0f;
+    
+    public float jitter = 0.1f;
+    public float jitterTimer;
+    public float jitterSpeed = 3;
+
+    void Start() {
+        _camera = Camera.main;
+        _transform = GetComponent<Transform>();
+        BoxCollider2D coll = GetComponentInParent<BoxCollider2D>();
+        body = new Rect(_transform.position, coll.size);
+    }
+
+    void FixedUpdate() {
+        _pos = _transform.position;
+
+        this.body.position = _pos - this.body.size * 0.5f;
+
+        Vector2 mPos = _camera.ScreenToWorldPoint(Input.mousePosition);
+        _diff = DistancePointToRectangle(mPos, body);
+
+
+        jitterTimer += Time.deltaTime * jitterSpeed;
+        if (jitterTimer > Mathf.PI * 2) jitterTimer = 0;
+        if (_diff < minDiff) {
+            _transform.localScale = Vector2.one * getScale(_diff) + Vector2.one * getJitter() ;
+        } else if (!_transform.localScale.Equals(Vector3.zero)) {
+            _transform.localScale = Vector3.zero;
+        }
+    }
+
+    float getScale(float diff) {
+        return minScale + (maxScale - minScale) * ((minDiff - diff) / (minDiff - maxDiff));
+    }
+
+    float getJitter() {
+        return jitter * Mathf.Sin(jitterTimer);
+    }
+
+    float DistancePointToRectangle(Vector2 point, Rect rect) {
+        if (point.x < rect.xMin) {
+            if (point.y < rect.yMin) {
+                Vector2 diff = point - new Vector2(rect.xMin, rect.yMin);
+                return diff.magnitude;
+            }
+
+            if (point.y > rect.yMax) {
+                Vector2 diff = point - new Vector2(rect.xMin, rect.yMax);
+                return diff.magnitude;
+            }
+
+            return rect.xMin - point.x;
+        }
+
+        if (point.x > rect.xMax) {
+            if (point.y < rect.yMin) {
+                Vector2 diff = point - new Vector2(rect.xMax, rect.yMin);
+                return diff.magnitude;
+            }
+
+            if (point.y > rect.yMax) {
+                Vector2 diff = point - new Vector2(rect.xMax, rect.yMax);
+                return diff.magnitude;
+            }
+
+            return point.x - rect.xMax;
+        }
+
+        if (point.y < rect.yMin) {
+            return rect.yMin - point.y;
+        }
+
+        if (point.y > rect.yMax) {
+            return point.y - rect.yMax;
+        }
+
+        return 0f;
+    }
+
+//    [ExecuteInEditMode]
+//    void OnDrawGizmosSelected() {
+//        Handles.DrawSolidRectangleWithOutline(body, Color.clear, Color.cyan);
+//    }
+}
---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour {
+    private Rigidbody2D _rigidbody;
+    public float speed = 0.8f;
+    
+    void Start() {
+        this._rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        Vector3 pos = this._rigidbody.position;
+        this._rigidbody.position = new Vector3(pos.x + Time.deltaTime * this.speed, pos.y);
+    }
+}
